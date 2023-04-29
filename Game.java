54d48a3982df2f95a1cc3fce63b741cd7924e00e
diomnne <dionnemaglasang@gmail.com>
2023-04-29 16:58:21 +0800

    updated Game class

--- Game.java
+++ Game.java
@@ -1,11 +1,28 @@
+import java.util.Scanner;
+
 public class Game
 {
 	public static void main (String[] args)
 	{
+		Scanner s = new Scanner(System.in);
 		Control control = new Control();
 
-		control.attackEnemy(control.enemy);
-		control.attackPlayer(control.player);
+		System.out.println("[A]Attack enemy\n[B]Attack player\n");
+
+		while(s.hasNext())
+		{
+			String key = s.nextLine();
+
+			if(key.equalsIgnoreCase("A"))
+			{
+				control.attackEnemy(control.enemy);
+			}
+
+			if(key.equalsIgnoreCase("B"))
+			{
+				control.attackPlayer(control.player);
+			}
+		}
 
 	}
 }
